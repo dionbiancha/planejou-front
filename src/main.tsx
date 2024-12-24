d90274624d89dad99a641724793b81aa74afb1fb
Dionei Bianchati <dion.biancha@gmail.com>
2024-12-24 08:17:45 -0300

    feat: analitcs

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import App from "./App.tsx";
 import "./index.css";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./translation/i18n.ts";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <I18nextProvider i18n={i18n}>
+    <Analytics />
     <App />
   </I18nextProvider>
 );
